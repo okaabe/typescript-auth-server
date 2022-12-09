da43fd41d3fd57ca7981ae644c4781c1d06bbee9
tosuki <okaabe2006@gmail.com>
2022-12-09 12:25:24 -0300

    Added prisma error helpers - Added a way to check if an error is an error known by prisma client - Added an enum with some PrismaClientRequestError codes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime"
+import { logger } from "../logger"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: [{ emit: "event", level: "query" }, "info", "warn", "error"],
+})
+prisma.$on("query", (e: any) => {
+  logger.debug("Query: " + e.query)
+  logger.debug("Params: " + e.params)
+  logger.debug("Duration: " + e.duration + "ms")
+})
+
+export enum PrismaKnownRequestErrorCode {
+  UniqueConstraintFailed = "P2002",
+  RecordDoesNotExist = "P2001",
+}
+
+export const isPrismaClientKnownRequestError = (err: any) =>
+  err instanceof PrismaClientKnownRequestError
 
 export default prisma
